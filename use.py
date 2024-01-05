a98ee6bf60384a2a4c3eba471a248e30b111a8e4
tuomina9 <akseli.tuominen@aalto.fi>
2024-01-05 20:12:19 +0200

    Small edits

--- use.py
+++ use.py
@@ -5,7 +5,7 @@ import librosa
 model = load_model('model.h5')
 
 # Load the audio file
-filename = "/Users/akselituominen/Desktop/sound2.wav"
+filename = "/Users/akselituominen/Desktop/sound4.wav"
 y, sr = librosa.load(filename, duration=3, offset=0.5)
 
 # Extract the MFCCs and take the mean
@@ -17,8 +17,13 @@ mfcc = np.expand_dims(mfcc, axis=-1)
 
 # Make a prediction
 prediction = model.predict(np.array([mfcc]))
-
+categories = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'ps', 'sad']
 # The prediction is a 7-element vector of probabilities. To get the predicted class, take the index of the highest probability.
 predicted_class = np.argmax(prediction)
 
-print("The predicted class is:", prediction)
+print("The predicted class is:", categories[predicted_class])
+print("The predictions for each category are:")
+for i in range(len(categories)):
+    category = categories[i]
+    probability = prediction[0][i]
+    print(category, ":", probability)
